a5250ea5d0015db45f618ce8c6dfc0ed85c33ae1
kzk-maeda <casino.music0711@gmail.com>
2020-02-05 23:44:59 +0900

    pytest

--- pytest/test_calcuration.py
+++ pytest/test_calcuration.py
@@ -1,25 +1,51 @@
+import pytest
 import unittest
 import calcuration
 
 release_name = 'dev'
 
-class CalTest(unittest.TestCase):
-    def setUp(self):
-        print('set up')
-        self.cal = calcuration.Cal()
+# class CalTest(unittest.TestCase):
+#     def setUp(self):
+#         print('set up')
+#         self.cal = calcuration.Cal()
 
-    def tearDown(self):
-        print('tear down')
+#     def tearDown(self):
+#         print('tear down')
+#         del self.cal
+
+#     # @unittest.skip('skip')
+#     @unittest.skipIf(release_name=='test', 'skip test!')
+#     def test_add_num_and_double(self):
+#         self.assertEqual(
+#             self.cal.add_num_and_double(1, 1),
+#             4
+#         )
+        
+#     def test_add_num_and_double_raise(self):
+#         with self.assertRaises(ValueError):
+#             self.cal.add_num_and_double('1', '1')
+
+class TestCal(object):
+    @classmethod
+    def setup_class(cls):
+        cls.cal = calcuration.Cal()
+
+    @classmethod
+    def teardown_class(cls):
+        del cls.cal
+
+    def setup_method(self, method):
+        print('method={}'.format(method.__name__))
+        self.cal = calcuration.Cal()
+    
+    def teardown_method(self, method):
+        print('method={}'.format(method.__name__))
         del self.cal
 
-    # @unittest.skip('skip')
-    @unittest.skipIf(release_name=='test', 'skip test!')
     def test_add_num_and_double(self):
-        self.assertEqual(
-            self.cal.add_num_and_double(1, 1),
-            4
-        )
-        
+        assert self.cal.add_num_and_double(1, 1) == 4
+    
+    @pytest.mark.skip(reason='skip')
     def test_add_num_and_double_raise(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             self.cal.add_num_and_double('1', '1')
